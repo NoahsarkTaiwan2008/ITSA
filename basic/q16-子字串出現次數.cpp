7aa9f76b08d0eee4405565bb2d51a34b3a17733f
Hikari <ben109092ben@gmail.com>
2025-05-26 10:18:27 +0800

    upload

---
+++ basic/q16-子字串出現次數.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string a;
+    cin >> a;
+
+    string b;
+    cin >> b;
+
+    size_t n = 0;
+    int count = 0;
+    while ((n = b.find(a, n)) != string::npos) {
+        ++count;
+        n++;
+    }
+
+    cout << count << endl;
+}
